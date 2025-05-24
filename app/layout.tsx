ce59512a7de0e9b98fd3bf46788989b0b810a53f
MrLad01 <ladayodeji@gmail.com>
2025-05-24 20:32:13 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
